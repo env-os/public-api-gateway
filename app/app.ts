ffd60cc134307d24df94067b3bf9beb9cd9163b8
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-06-05 11:18:13 +0200

    Install routing controllers, typedi and axios packages.

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,13 @@
-import express from 'express'
+import 'reflect-metadata';
+import { createExpressServer, useContainer } from 'routing-controllers';
+import { Container } from 'typedi';
+
+useContainer(Container);
 
 const port = process.env.PORT || 3000;
-const app: express.Application = express();
-
+const app = createExpressServer({
+    controllers: []
+})
 
 app.listen(port, function () {
   console.log(`Public API Gateway listening on port ${port}!`);
