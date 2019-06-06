8022d7895152c1e53937147b777728de368005d8
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-06-06 10:28:52 +0200

    Change HttpError in BadRequestError

--- app/controllers/user.controller.ts
+++ app/controllers/user.controller.ts
@@ -27,7 +27,7 @@ export class UserController {
         LogsUtil.logRequest(req);
         await this.userService.delete(uuid)
         .catch(() => {
-            throw new HttpError(500);
+            throw new BadRequestError();
         })
     }
 
@@ -37,7 +37,7 @@ export class UserController {
         LogsUtil.logRequest(req);
         return await this.userService.getAll()
         .catch(() => {
-            throw new HttpError(500);
+            throw new NotFoundError();
         })
     }
 
